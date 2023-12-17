f5f0b24d29892dc0e35a64e8d01d2942e4d8ad2d
don <donpattamana@gmail.com>
2023-12-17 15:22:11 +0530

    first commit

---
+++ src/screens/HomePageStatic/HomePageStatic.jsx
@@ -0,0 +1,272 @@
+import React from "react";
+import "./style.css";
+
+export const HomePageStatic = () => {
+  return (
+    <div className="home-page-static">
+      <header className="header">
+        <button className="button">
+          <div className="overlap-group">
+            <div className="div-button-down" />
+            <div className="div-button-up">
+              <div className="text-wrapper">About Me</div>
+            </div>
+          </div>
+        </button>
+        <img className="dp" alt="Dp" src="/img/dp.svg" />
+      </header>
+      <div className="body">
+        <div className="home">
+          <p className="hai-i-am-don-shaji">
+            Hai I am Don Shaji , UI/UX &amp; graphic designer , frontend developer and programmer from India, Kerala
+          </p>
+          <img className="unsplash" alt="Unsplash" src="/img/unsplash-vlbc1scnrng.png" />
+          <button className="overlap-group-wrapper">
+            <div className="overlap-group">
+              <div className="div-button-down" />
+              <div className="div-button-up">
+                <div className="text-wrapper">Contact Me</div>
+              </div>
+            </div>
+          </button>
+          <div className="group">
+            <div className="overlap">
+              <div className="c-digo">CÓDIGO</div>
+              <div className="dise-o">DISEÑO</div>
+            </div>
+          </div>
+        </div>
+        <div className="frame">
+          <div className="div">Expert In</div>
+          <div className="group-wrapper">
+            <div className="group-2">
+              <div className="tile-6">
+                <div className="text-wrapper-8">JavaScript</div>
+                <img className="img" alt="Icon javascript JS" src="/img/icon-javascript-js.png" />
+              </div>
+              <div className="tile-7">
+                <div className="text-wrapper-8">HTML CSS</div>
+                <img className="icon-code-file" alt="Icon code file" src="/img/icon-code-file.png" />
+              </div>
+              <div className="tile-8">
+                <div className="text-wrapper-9">Python</div>
+                <img className="img-2" alt="Icon file type" src="/img/icon-file-type-python.png" />
+              </div>
+              <div className="tile-9">
+                <div className="text-wrapper-10">Photoshop</div>
+                <img className="img-2" alt="Adobe photoshop CC" src="/img/adobe-photoshop-cc-icon-1.svg" />
+              </div>
+              <div className="overlap-wrapper">
+                <div className="overlap-2">
+                  <div className="text-wrapper-8">C++</div>
+                  <img className="c-logo-wine" alt="C logo wine" src="/img/c-logo-wine-1.svg" />
+                </div>
+              </div>
+              <div className="tile-10">
+                <div className="text-wrapper-11">Figma</div>
+                <img className="icon-figma" alt="Icon figma" src="/img/icon-figma.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mid">
+          <div className="element">
+            <div className="text">
+              <p className="p">
+                Developed Scripts and programs.
+                <br />
+                Skilled in programing concepts
+              </p>
+            </div>
+            <div className="title">
+              <div className="text-wrapper-12">Programing</div>
+            </div>
+            <img className="image" alt="Image" src="/img/image.png" />
+          </div>
+          <div className="element-2">
+            <div className="text">
+              <p className="p">
+                Very capable in developing
+                <br />
+                Front-End of Web&nbsp;&nbsp;development
+              </p>
+            </div>
+            <div className="title-2">
+              <div className="text-wrapper-12">Front-End</div>
+            </div>
+            <div className="image-2" />
+          </div>
+          <div className="element-3">
+            <div className="text">
+              <p className="p">Designed full fledged UI for UDGAM HomeShop</p>
+            </div>
+            <div className="title-3">
+              <div className="text-wrapper-12">UI Design</div>
+            </div>
+            <div className="image-3" />
+          </div>
+          <div className="element-4">
+            <div className="text">
+              <p className="p">
+                Designed UI/UX for SkedU Student <br />
+                learning platform
+              </p>
+            </div>
+            <div className="title-4">
+              <div className="text-wrapper-12">UX Experience</div>
+            </div>
+            <img className="image" alt="Image" src="/img/image-1.png" />
+          </div>
+          <p className="text-wrapper-13">
+            Every designer needs the right tools to do the perfect job. Thankfully, we can do that. I design products
+            that are more than pretty. I make them shippable and usable.
+          </p>
+          <p className="giving-your-project">
+            Giving Your Project
+            <br />
+            Some Great Ideas
+          </p>
+          <div className="text-wrapper-14">FEATURING</div>
+          <button className="button-2">
+            <div className="overlap-group">
+              <div className="div-button-down" />
+              <div className="div-button-up">
+                <div className="contact-me">More About</div>
+              </div>
+            </div>
+          </button>
+        </div>
+        <div className="section">
+          <p className="text-wrapper-15">Some Of My Best Works</p>
+          <div className="portfolio">PORTFOLIO</div>
+          <button className="button-3">
+            <div className="overlap-group">
+              <div className="div-button-down" />
+              <div className="div-button-up">
+                <div className="contact-me-2">View All</div>
+              </div>
+            </div>
+          </button>
+          <img className="rectangle" alt="Rectangle" src="/img/rectangle-44.png" />
+          <img className="rectangle-2" alt="Rectangle" src="/img/rectangle-46.png" />
+          <img className="rectangle-3" alt="Rectangle" src="/img/rectangle-48.png" />
+          <img className="rectangle-4" alt="Rectangle" src="/img/rectangle-47.svg" />
+          <img className="rectangle-5" alt="Rectangle" src="/img/rectangle-49.png" />
+          <img className="rectangle-6" alt="Rectangle" src="/img/rectangle-45.png" />
+          <img className="rectangle-7" alt="Rectangle" src="/img/rectangle-38.svg" />
+          <img className="rectangle-8" alt="Rectangle" src="/img/rectangle-43.png" />
+          <img className="rectangle-9" alt="Rectangle" src="/img/rectangle-39.svg" />
+        </div>
+        <div className="section-nav">
+          <p className="check-some-of-random">
+            Check Some of random <br />
+            Clicks
+          </p>
+          <div className="text-wrapper-16">LATEST BLOG</div>
+          <button className="button-4">
+            <div className="overlap-group">
+              <div className="div-button-down" />
+              <button className="contact-me-wrapper">
+                <div className="contact-me-3">Load More</div>
+              </button>
+            </div>
+          </button>
+          <div className="group-3">
+            <div className="frame-2" />
+            <div className="frame-3" />
+            <div className="frame-4" />
+          </div>
+          <div className="group-4">
+            <div className="frame-5" />
+            <div className="frame-6" />
+            <div className="frame-7" />
+          </div>
+          <div className="group-5">
+            <div className="frame-8" />
+            <div className="frame-9" />
+            <div className="frame-10" />
+          </div>
+        </div>
+        <div className="feedback-scetion">
+          <div className="text-2">
+            <p className="text-wrapper-17">
+              All Your Qustions are in A document, in question and answer format, that introduces newcomers to a topic
+              or answers common questions.
+            </p>
+          </div>
+          <div className="be-kind-to-your-mind-wrapper">
+            <p className="be-kind-to-your-mind">
+              Be Kind to Your Mind
+              <br />
+              Question &amp; Answer
+            </p>
+          </div>
+          <div className="FA-qs">FAQS</div>
+          <div className="faqs">
+            <div className="element-5">
+              <img className="arrow" alt="Arrow" src="/img/arrow-3.png" />
+              <div className="title-5">
+                <p className="text-wrapper-18">Need My help and Support ?</p>
+              </div>
+            </div>
+            <div className="element-6">
+              <img className="arrow" alt="Arrow" src="/img/arrow-3.png" />
+              <div className="title-6">
+                <p className="text-wrapper-18">Need a face to face talk ?</p>
+              </div>
+            </div>
+            <div className="element-7">
+              <img className="arrow" alt="Arrow" src="/img/arrow-3.png" />
+              <div className="title-7">
+                <p className="text-wrapper-18">What I am doing right now ?</p>
+              </div>
+            </div>
+            <div className="element-8">
+              <img className="arrow" alt="Arrow" src="/img/arrow-3.png" />
+              <div className="title-8">
+                <p className="text-wrapper-18">How can I help your ?</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bottam-nav">
+          <div className="overlap-3">
+            <img className="rectangle-10" alt="Rectangle" src="/img/rectangle-2553.png" />
+            <img className="ellipse" alt="Ellipse" src="/img/ellipse-15.png" />
+          </div>
+          <div className="group-6">
+            <p className="have-you-project-in">HAVE YOU PROJECT IN MIND?</p>
+            <div className="let-s-make-something-wrapper">
+              <div className="let-s-make-something">
+                Let’s Make Something
+                <br />
+                Great Together!
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="insta-parent">
+          <a className="insta" href="https://www.instagram.com/don.shaji/" target="_blank">
+            <img className="insta-child" alt="" src="/frame-162700@2x.png" />
+          </a>
+          <a className="github" href="https://github.com/Donshaji" target="_blank">
+            <img className="vector-icon" alt="" src="/vector@2x.png" />
+          </a>
+          <a className="linkedin" href="https://www.linkedin.com/in/donshaji/" target="_blank">
+            <img className="linkedin-child" alt="" src="/frame-162699@2x.png" />
+          </a>
+          <div className="property-1default" style={{ textDecoration: "none", position: "absolute", top: "0px", left: "522px" }}>
+            <div className="property-1default-inner">
+              <img className="frame-child" alt="" src="/frame-162701@2x.png" />
+            </div>
+          </div>
+        </div>
+        <footer className="footer">
+          <div className="rectangle-11" />
+          <div className="text-wrapper-19">Designed by doN</div>
+          <div className="text-wrapper-20">Developed by doN</div>
+        </footer>
+      </div>
+    </div>
+  );
+};
---
+++ src/screens/HomePageStatic/index.js
@@ -0,0 +1,1 @@
+export { HomePageStatic } from "./HomePageStatic";
