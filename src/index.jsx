30e9d19319baca1c87027bd8e989c9d27985b23e
don <donpattamana@gmail.com>
2023-12-25 14:02:50 +0530

    test

--- src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { HomePageStatic } from "./screens/HomePageStatic";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<HomePageStatic />);
--- src/screens/HomePageStatic/index.js
+++
@@ -1,1 +0,0 @@
-export { HomePageStatic } from "./HomePageStatic";
